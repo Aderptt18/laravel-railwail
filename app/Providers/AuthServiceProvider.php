f63dcc6b5a2a0c246ba8b1b6b00f9ba4b601f5de
Aderptt18 <142180561+Aderptt18@users.noreply.github.com>
2024-10-08 11:14:34 -0500

    politias

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,24 @@
 
 namespace App\Providers;
 
+
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Policies\ClientesPolicy;
+use App\Policies\ProductosPolicy;
+use App\Policies\ComprasPolicy;
+use App\Policies\ProveedoresPolicy;
+use App\Policies\UsersPolicy;
+use App\Policies\RolesPolicy;
+use App\Policies\VentasPolicy;
+
+use App\Models\Clientes;
+use App\Models\Compras;
+use App\Models\Productos;
+use App\Models\Proveedores;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Ventas;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +29,14 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Clientes::class => ClientesPolicy::class,
+        Productos::class => ProductosPolicy::class,
+        Compras::class => ComprasPolicy::class,
+        Proveedores::class => ProveedoresPolicy::class,
+        Role::class => RolesPolicy::class,
+        User::class => UserPolicy::class,
+        Ventas::class => VentasPolicy::class,
+
     ];
 
     /**
