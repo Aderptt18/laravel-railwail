d1fd27e78b08c6327003f8e50d034a47ad1b9ed3
Aderptt18 <142180561+Aderptt18@users.noreply.github.com>
2024-10-13 22:17:25 -0500

    crear super usuario

---
+++ database/seeders/SuperUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+
+class SuperUserSeeder extends Seeder
+{
+    public function run()
+    {
+        // Crear el usuario
+        $user = User::create([
+            'name' => 'And',
+            'email' => 'and@.com',
+            'password' => Hash::make('and'), // Cambia la contraseña
+        ]);
+
+        // Verificar si el rol 'Super' ya existe, si no, lo crea
+
+        // Asignar el rol al usuario
+        $user->assignRole('Super');
+    }
+}
